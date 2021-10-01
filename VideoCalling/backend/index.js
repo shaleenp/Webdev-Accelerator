40e12c1782d78a6518459e3da8008cb731a3d963
somil24 <somil2402@gmail.com>
2021-10-02 00:41:12 +0530

    Added video calling folder

---
+++ VideoCalling/backend/index.js
@@ -0,0 +1,36 @@
+const app =require('expres')()
+const server=require('http').createServer(app)
+const cors=require('cors')
+
+const io=require('socket.io')(server,{
+    cors:{
+        origin:"*",
+        methods:["GET","POST"]
+    }
+})
+
+app.use(cors())
+
+const PORT=proces.env.PORT||5000 
+
+app.get("/",(req,res)=>{
+    res.send('Server is running')
+})
+
+//When connection message is sent from client
+io.on('connection',socket=>{
+    socket.emit('me',socket.id)
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('callEnded')
+    })
+
+    socket.on('callUser',({userToCall,signalData,from,name})=>{
+        io.to(userToCall).emit('callUser',{signal:signalData,from,name})
+    })
+
+    socket.on("answerCall",(data)=>{
+        io.to(data.to).emit("callAccepted",data.signal)
+    })
+})
+
+server.listen(PORT,()=>console.log('server listening on port 1ko5'))
